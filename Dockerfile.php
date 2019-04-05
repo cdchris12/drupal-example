3bf79178dce80ae57d5d9102a79191417fc5a16c
Chris Davis <cdchris12@gmail.com>
2019-04-05 15:04:58 -0500

    Fixing ldap module

--- Dockerfile.php
+++ Dockerfile.php
@@ -2,6 +2,13 @@ ARG CLI_IMAGE
 FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:7.2-fpm
-RUN apk update && apk add php-ldap
+RUN \ 
+    apk update && \
+    apk add libldap && \
+    apk add openldap-dev && \
+    apk add php7-ldap 
+
+RUN \
+    docker-php-ext-install ldap
 
 COPY --from=cli /app /app
